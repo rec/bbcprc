ab1f76f7202b92765427a6a9d52cc5e3d8d40143
Tom Ritchford <tom@swirly.com>
2018-04-22 11:52:39 +0200

    Split durations

--- bbcsfx/constants.py
+++ bbcsfx/constants.py
@@ -4,5 +4,6 @@ OUTPUT_DIR = '/Volumes/Bach/disks/bbcsfx'
 URL_ROOT = 'http://bbcsfx.acropolis.org.uk/assets'
 ALL_FILENAMES = 'results/all_filenames.txt'
 DURATION_FILE = 'results/durations.json'
+METADATA_DIR = 'results/metadata'
 
 FRAME_RATE = 44100
