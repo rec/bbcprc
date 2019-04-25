2e045d42e39cf01605b7b8dba96b02f304ceef26
Tom Ritchford <tom@swirly.com>
2019-04-25 16:36:37 +0200

    More results

--- bbcprc/merge.py
+++ bbcprc/merge.py
@@ -1,15 +1,18 @@
 """
 Merges many .WAV files into one great big one and keeps track of the sample
-offset for each one
+offset for each one.
 """
 
 import sys, yaml
 from . import files
+STOP_AFTER = 10
 
 
 def merge(directory, outfile, nframes=0, delete_on_fail=False):
     def records(out):
-        for f in files.wave_files(directory):
+        for i, f in enumerate(files.wave_files(directory)):
+            if STOP_AFTER and i >= STOP_AFTER:
+                break
             yield f.name, out.tell()
             out.writeframes(files.read_frames(f))
         yield '(END)', out.tell()
