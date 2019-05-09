056b53c4724d064ba540f75794a782426fe4be83
Tom Ritchford <tom@swirly.com>
2019-05-09 14:06:00 +0200

    Make corpus_to_float actually work

--- bbcprc/extract/corpus_to_float.py
+++ bbcprc/extract/corpus_to_float.py
@@ -1,12 +1,22 @@
 from .. data import READ, WRITE
+from icecream import ic
+
+DIVISOR = 1
 
 
 def corpus_to_float():
+    ic()
     corpus = READ.corpus()
-    outfile = WRITE.corpus.float32(shape=len(corpus), dtype='float32')
-    outfile[:] = corpus[:, 0]
-    outfile[:] += corpus[:, 1]
+    ic()
+    length = len(corpus) // DIVISOR
+    outfile = WRITE.corpus.float32(shape=length, dtype='float32')
+    ic()
+    outfile[:] = corpus[:length, 0]
+    ic()
+    outfile[:] += corpus[:length, 1]
+    ic()
     outfile /= 0x10000
+    ic()
 
 
 if __name__ == '__main__':
